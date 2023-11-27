f4efbe7227914ab2d80473528c77aee7f840fc9e
Himel Majumder <himelmajumder1@gmail.com>
2023-11-27 23:19:11 +0100

    Refactor ArtistsCard component animations

--- src/components/ArtistsCard.jsx
+++ src/components/ArtistsCard.jsx
@@ -1,23 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
-const container = {
-  hidden: { opacity: 1, scale: 0 },
-  visible: {
-    opacity: 1,
-    scale: 1,
-    transition: {
-      delayChildren: 0.3,
-      staggerChildren: 0.2,
-    },
-  },
-};
-const item = {
-  hidden: { y: 20, opacity: 0 },
-  visible: {
-    y: 0,
-    opacity: 1,
-  },
-};
+
+import {container, item} from "../utils/Animations";
 
 export function ArtistsCard({ artists }) {
   const navigate = useNavigate();
